4f8014751bc82ba4bcf0defd4acc1526c5cc4eb2
Zack Jones <zackjones11@gmail.com>
2021-04-06 11:51:11 +0200

    implements hook in example file

--- examples/stories/Datepicker.tsx
+++ examples/stories/Datepicker.tsx
@@ -1,7 +1,20 @@
 import * as React from "react";
+import { useDatepicker } from "../../src/index";
+import "./Datepicker.css";
 
 const Datepicker: React.FC = () => {
-  return <div>test</div>;
+  const { calendar } = useDatepicker({ currentDate: new Date() });
+  return (
+    <div className="calendar-wrapper">
+      <div>
+        <ol className="calendar">
+          {calendar.map((day) => {
+            return <li key={day}>{day}</li>;
+          })}
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 export default Datepicker;
