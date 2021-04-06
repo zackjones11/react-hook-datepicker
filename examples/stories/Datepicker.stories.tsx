ae5754df0413533cad48ffeeca47917cfae294cb
Zack Jones <zackjones11@gmail.com>
2021-04-06 11:29:55 +0200

    basic docs/examples setup with storybook

---
+++ examples/stories/Datepicker.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Datepicker from "./Datepicker";
+
+export default {
+  title: "Example/Datepicker",
+  component: Datepicker,
+};
+
+const Template: React.FC = () => <Datepicker />;
+
+export const Basic = Template.bind({});
